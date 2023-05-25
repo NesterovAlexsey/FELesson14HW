742225d3b3ece4ee47501fffe5cc5091ebc754e6
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-25 16:59:41 +0200

    connected html with API

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // Задание 2 *дополнительно
 // Выберите одну API из списка. Сделайте fetch запрос, 
 // разместите полученную от API информацию на странице html.
+
+const whatshouldIDo = document.getElementById("randomTask");
+const refreshBtn = document.getElementById("nextTask");
+
+async function getNewMission() {
+
+    const responce = await fetch("https://www.boredapi.com/api/activity");
+    const resultJson = await responce.json();
+    const {activity} = resultJson;
+
+    whatshouldIDo.textContent = activity;
+}
+
+getNewMission();
+refreshBtn.addEventListener('click', getNewMission);
