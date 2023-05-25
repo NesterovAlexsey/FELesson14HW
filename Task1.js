bd1ec00371dfdaac5bf0532fd3b757484824a610
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-25 15:38:40 +0200

    first version for resolved task1

--- Task1.js
+++ Task1.js
@@ -1,11 +1,17 @@
 // Задание 1
 // Имеется объект
 
-// const obj = {
-// products: [
-//     {title: "bottle of wine", price: 35, category: "wines"},
-//     {title: "1984", price: 15, category: "books", author: "George Orwell"},
-//     {title: "Iphone XR", price: 400, category: "phones", campera: "12MP"}
-//     ]
-// };
+const obj = {
+products: [
+    {title: "bottle of wine", price: 35, category: "wines"},
+    {title: "1984", price: 15, category: "books", author: "George Orwell"},
+    {title: "Iphone XR", price: 400, category: "phones", campera: "12MP"}
+    ]
+};
+
 // Используя деструктуризацию пулучите в отдельную переменную значение поля author;
+
+const {products} = obj;
+const [,secondPosition] = products;
+const {author} = secondPosition;
+console.log(author);
