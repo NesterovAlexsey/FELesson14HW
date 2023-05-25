9cee7e0c67f3945ef3a34970798dfcd19284b2a0
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-25 15:20:33 +0200

    Initial commit

---
+++ Task1.js
@@ -0,0 +1,11 @@
+// Задание 1
+// Имеется объект
+
+// const obj = {
+// products: [
+//     {title: "bottle of wine", price: 35, category: "wines"},
+//     {title: "1984", price: 15, category: "books", author: "George Orwell"},
+//     {title: "Iphone XR", price: 400, category: "phones", campera: "12MP"}
+//     ]
+// };
+// Используя деструктуризацию пулучите в отдельную переменную значение поля author;
